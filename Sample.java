ae8c7e107a1a28b70862155b9dd3646bf65d5988
sairajeshd <sairajeshd@users.noreply.github.com>
2021-06-16 18:35:24 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ public class Sample{
      public static void main(String []args){
         System.out.println("Hello World");
         System.out.println("Welcome to the world of jenkins");
-        System.out.println("Creatign a job with pipeline");
+        System.out.println("Creating a job with pipeline");
+        System.out.println("Bye........");
      }
 }
